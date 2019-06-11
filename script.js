39d26dda9021e1e050ed61780a89b33e1349e94c
Maciej Biełous <bialywht@gmail.com>
2019-06-11 16:25:39 +0200

    Added classes to h1 p, added welcome script

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const name = 'Maciej';
 const age = 39;
-console.log(name);
-console.log(age);
-console.log(`Nazywam się ${name} i mam ${age} lat.`);
+
+alert(`Nazywam się ${name} i mam ${age} lat.`);
