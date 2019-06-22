4f9797a9beb164749c130207c972ae6b76af9385
Maciej Biełous <bialywht@gmail.com>
2019-06-22 23:46:01 +0200

    trying scriptimg

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const name = 'Maciej';
 const age = 39;
 
 console.log(`Nazywam się ${name} i mam ${age} lat.`);
+
+const heading = document.querySelector('#top');
+console.log(heading.innerHTML);
+
+heading.innerHTML = "<h1>Zupełnie inny tekst</h1>";
+
+console.log(heading.innerHTML);
